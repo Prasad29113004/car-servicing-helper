b675caffefbc7fc1e1d3d7040c004dd15ac2a2e3
Prasad Nimje <165168775+Prasad29113004@users.noreply.github.com>
2026-06-29 19:40:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Add this line below! Replace with your exact repo name.
+  base: '/car-servicing-helper/', 
+})
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
